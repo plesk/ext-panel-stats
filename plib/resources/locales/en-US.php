6c1dd66bc718a3b8f58547f89a4006d72c74a89f
Eugene Kazakov <ekazakov@parallels.com>
2014-06-03 11:32:21 +0700

    Locale fixes.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,7 +1,7 @@
 <?php
 
 $messages = array(
-    'settingsPageTitle' => 'Panel Statistics Settings',
-    'settingsSaved' => 'Settings were saved.',
-    'formSettingsDescription' => 'You can protect statistics information or make it public available.',
+    'settingsPageTitle' => 'Plesk Statistics Settings',
+    'settingsSaved' => 'The settings were saved.',
+    'formSettingsDescription' => 'You can protect statistics information or make it publicly available.',
 );
